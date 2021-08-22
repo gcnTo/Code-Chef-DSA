cbd799240f4f0acf7ebd28e00b9449cb71344728
gcnTo <60984509+gcnTo@users.noreply.github.com>
2021-08-22 19:13:43 +0300

    Update coin_flip.py

--- coin_flip.py
+++ coin_flip.py
@@ -5,10 +5,13 @@ for i in range(t):
     
     for j in range(g):
         i,n,q = map(int,input().split())
+        
+        # If n is even, Heads = Tails no matter what according to the problem statement.
         if n % 2 == 0:
             heads = n // 2
             tails = n - heads
         else:
+        # If n is odd, "Number of Heads will be less than Number of Tails, if all coins were Heads initially" and vice versa
             if i == 1:
                 heads = n // 2
                 tails = n - heads
