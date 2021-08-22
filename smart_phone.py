5e62485256b3244b960ad608b411d1f8be5a72a9
gcnTo <60984509+gcnTo@users.noreply.github.com>
2021-08-22 08:41:34 +0300

    Update smart_phone.py

--- smart_phone.py
+++ smart_phone.py
@@ -1,19 +1,19 @@
 n = int(input())
-l = []
+budget = []
 
 # Populating the list of customer budgets.
 for i in range(n):
-    l.append(int(input()))
+    budget.append(int(input()))
 
-l = sorted(l) #Sorts the list l for efficiency.
+budget = sorted(budget) #Sorts the list of budgets for efficiency.
 top = 0
 
-# Since l is sorted, we know that each l[i] < l[i+1]
-# And since we only have a deal if l[i] <= customer budget,
-# we know that each l[i] needs to be multiplied by (n-i) elements, 
-# because all the budgets ((n-i) budgets) following l[i] are more than l[i]
+# Since the list budget is sorted, we know that each budget[i] < budget[i+1]
+# And since we only have a deal if budget[i] <= customer budget,
+# and all the budgets ((n-i) budgets) following budget[i] are more than budget[i]
+# we know that each budget[i] needs to be multiplied by (n-i) elements, 
 for i in range(n):
-    if l[i] * (n-i) > top: 
-        top = l[i] * (n-i)
+    if budget[i] * (n-i) > top: 
+        top = budget[i] * (n-i)
 
 print(top)
